66c79b3ee09720a22c632a84e3060f51e9bea304
guilherme.sabino <guilherme.sabino@icolabora.com>
2019-05-07 12:16:24 -0300

    commit

--- casadocodigo/server.js
+++ casadocodigo/server.js
@@ -1,3 +1,14 @@
 const http = require('http');
 
-const servidor = http.createServer();
+const servidor = http.createServer(function (req, resp) {
+    resp.end(`<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Casa do Código</title>
+    </head>
+    <body>
+        <h1>Casa do código</h1>
+    </body>
+    </html>`)
+});
+servidor.listen(3000);
