8c46b833286bb3b89773f0eca98ae117204fdf7b
guilherme.sabino <guilherme.sabino@icolabora.com>
2019-05-07 14:53:04 -0300

    commit

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -1,3 +1,6 @@
 // Função express para criar abstrações de rotas
 const express = require('express');
 const app = express();
+
+// Dizendo ao node que o app será exportado para ser utilizado em outros locais
+module.exports = app;
