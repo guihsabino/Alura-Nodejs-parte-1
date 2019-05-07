e6de172a546c9cd7f831993c8df4abc48e1d7486
guilherme.sabino <guilherme.sabino@icolabora.com>
2019-05-07 16:27:31 -0300

    commit

--- casadocodigo/server.js
+++ casadocodigo/server.js
@@ -1,6 +1,5 @@
-const app = require('../src/config/custom-express');
+const app = require('./src/config/custom-express');
 
-// Função que ouve a porta 3000
-app.listen(3000, function () {
-    console.log('Servidor rodando na porta 3000');
+app.listen(3000, function() {
+    console.log(`Servidor rodando na porta 3000`);
 });
