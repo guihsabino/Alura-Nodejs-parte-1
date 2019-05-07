2b174039290afe7e54c4c9a18333c742db8533c8
guilherme.sabino <guilherme.sabino@icolabora.com>
2019-05-07 14:42:16 -0300

    commit

--- casadocodigo/server.js
+++ casadocodigo/server.js
@@ -31,36 +31,3 @@ app.get('/livros', function (req, resp) {
             </html>`);
 });
 
-// const http = require('http');
-
-// const porta = 3000;
-
-// const servidor = http.createServer(function (req, resp) {
-
-//     let html = '';
-//     if (req.url == '/') {
-
-//         html = `<html>
-//         <head>
-//             <meta charset="UTF-8">
-//             <title>Homepageo</title>
-//         </head>
-//         <body>
-//             <h1>Casa do código</h1>
-//         </body>
-//         </html>`
-//     } else if (req.url == '/livros') {
-
-//         html = `<html>
-//             <head>
-//                 <meta charset="UTF-8">
-//                 <title>Casa do Código</title>
-//             </head>
-//             <body>
-//                 <h1>Listagem de Livros</h1>
-//             </body>
-//             </html>`
-//     }
-//     resp.end(html);
-// });
-// servidor.listen(porta);
