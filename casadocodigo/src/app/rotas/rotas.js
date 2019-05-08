660cfcfff031169eb585b57c947293840d1e26cc
guilherme.sabino <guilherme.sabino@icolabora.com>
2019-05-08 15:34:08 -0300

    commit

--- casadocodigo/src/app/rotas/rotas.js
+++ casadocodigo/src/app/rotas/rotas.js
@@ -2,7 +2,7 @@ const LivroDao = require('../infra/livro-dao');
 const db = require('../../config/database');
 
 module.exports = (app) => {
-    app.get('/', function(req, resp) {
+    app.get('/', function (req, resp) {
         resp.send(
             `
                 <html>
@@ -16,18 +16,27 @@ module.exports = (app) => {
             `
         );
     });
-    
-    app.get('/livros', function(req, resp) {
+
+    app.get('/livros', function (req, resp) {
 
         const livroDao = new LivroDao(db);
         livroDao.lista()
-                .then(livros => resp.marko(
-                    require('../views/livros/lista/lista.marko'),
-                    {
-                        livros: livros
-                    }
-                ))
-                .catch(erro => console.log(erro));
+            .then(livros => resp.marko(
+                require('../views/livros/lista/lista.marko'),
+                {
+                    livros: livros
+                }
+            ))
+            .catch(erro => console.log(erro));
+
+        app.get('/livros/form', function (req, resp) {
+
+            resp.marko(require('../views/livros/form/form.marko'))
+
+        })
+            .catch(erro => console.log(erro));
+
+
         // livroDao.lista(function(erro, resultados) {
 
         //     resp.marko(
