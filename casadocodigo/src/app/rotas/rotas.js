5c4351783af953db8d5ca35f3fff5fe379a38d2b
guilherme.sabino <guilherme.sabino@icolabora.com>
2019-05-08 16:39:20 -0300

    commit

--- casadocodigo/src/app/rotas/rotas.js
+++ casadocodigo/src/app/rotas/rotas.js
@@ -35,6 +35,10 @@ module.exports = (app) => {
         });
         app.post('/livros', function (req, resp) {
             console.log(req.body);
+            const livroDao = new LivroDao(db);
+            livroDao.adiciona(req.body)
+                .then(resp.redirect('/livros'))
+                .catch(erro => console.log(erro));
         });
 
         // Exemplo de resposta para o navegador
--- casadocodigo/src/config/custom-express.js
+++ casadocodigo/src/config/custom-express.js
@@ -13,3 +13,12 @@ const rotas = require('../app/rotas/rotas');
 rotas(app);
 
 module.exports = app;
+
+// Exemplo de código middleware
+// const express = require('express');
+// const app = express();
+
+// app.use('*', (req, res, next) => {
+
+//    // código do middleware.
+// });
