2a23dd395b040b79d9cf73186912c83f1f934e9d
guilherme.sabino <guilherme.sabino@icolabora.com>
2019-05-10 11:25:50 -0300

    commit

--- casadocodigo/src/app/views/livros/form/form.marko.js
+++ casadocodigo/src/app/views/livros/form/form.marko.js
@@ -31,9 +31,9 @@ function render(input, out, __component, component, state) {
 
   out.w("<div class=\"form-group\"><label for=\"titulo\">Titulo:</label><input type=\"text\" id=\"titulo\" name=\"titulo\" value=\"" +
     marko_escapeXmlAttr(data.livro.titulo) +
-    "\" placeholder=\"coloque o titulo\" class=\"form-control\"></div><div class=\"form-group\"><label for=\"preco\">Preço:</label><input type=\"text\" id=\"preco\" name=\"preco\" placeholder=\"150.25\" value=\"" +
+    "\" placeholder=\"Insira o título do livro\" class=\"form-control\"></div><div class=\"form-group\"><label for=\"preco\">Preço:</label><input type=\"text\" id=\"preco\" name=\"preco\" placeholder=\"150.25\" value=\"" +
     marko_escapeXmlAttr(data.livro.preco) +
-    "\" class=\"form-control\"></div><div class=\"form-group\"><label for=\"descricao\">Descrição:</label><textarea cols=\"20\" rows=\"10\" id=\"descricao\" name=\"descricao\" placeholder=\"fale sobre o livro\" class=\"form-control\">" +
+    "\" class=\"form-control\"></div><div class=\"form-group\"><label for=\"descricao\">Descrição:</label><textarea cols=\"20\" rows=\"10\" id=\"descricao\" name=\"descricao\" placeholder=\"Insira uma breve descrição sobre o livro\" class=\"form-control\">" +
     marko_escapeXml(data.livro.descricao) +
     "</textarea></div><input type=\"submit\" value=\"Salvar\" class=\"btn btn-primary\"></form></div></main><footer class=\"rodape\"><div class=\"container\"><div class=\"row align-items-center\"><div class=\"col-4\"><img src=\"/estatico/imagens/logo-rodape.svg\" class=\"logo-rodape\"></div><div class=\"col-8\"><ul class=\"redesSociais\"><li><a href=\"http://www.facebook.com/casadocodigo\" class=\"compartilhar-facebook\" target=\"_blank\">/CasaDoCodigo</a></li><li><a href=\"http://www.twitter.com/casadocodigo\" class=\"compartilhar-twitter\" target=\"_blank\">@casadocodigo</a></li></ul></div></div></div></footer>");
 
